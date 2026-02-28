abbc36372c5867ba332ec09016c06dc5d8a9d2dc
Boris Korobeinikov <slon2k@gmail.com>
2026-02-28 16:57:11 +0500

    Configure entity configurations to prevent database-generated keys for Event, Invitation, and OutboxMessage entities

--- src/backend/EventHub.Infrastructure/Persistence/Migrations/20260228114429_ConfigureClientGeneratedKeys.Designer.cs
+++ src/backend/EventHub.Infrastructure/Persistence/Migrations/20260228114429_ConfigureClientGeneratedKeys.Designer.cs
@@ -4,6 +4,7 @@ using EventHub.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EventHub.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(EventHubDbContext))]
-    partial class EventHubDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260228114429_ConfigureClientGeneratedKeys")]
+    partial class ConfigureClientGeneratedKeys
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/backend/EventHub.Infrastructure/Persistence/Migrations/20260228114429_ConfigureClientGeneratedKeys.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventHub.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigureClientGeneratedKeys : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
